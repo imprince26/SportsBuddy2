e22b53bd6f76766da408b2d06458c92e2246c9e0
imprince26 <princepatel260806@gmail.com>
2025-04-01 23:02:01 +0530

    update code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "None",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
     if (process.env.NODE_ENV === "production") {

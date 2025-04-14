b18eae02b3b5e0ff8d6295e957da7aa8e8ccd5f1
imprince26 <princepatel260806@gmail.com>
2025-04-14 21:23:45 +0530

    update code

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { isAuthenticated } from '../middleware/authMiddleware.js';
-import { upload } from '../middleware/uploadMiddleware.js';
+import express from "express";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 import {
   createEvent,
   getEventById,
@@ -11,22 +11,22 @@ import {
   addTeam,
   addRating,
   getUserEvents,
-} from '../controllers/eventController.js';
+} from "../controllers/eventController.js";
 
 const router = express.Router();
 
 // Public routes
-router.get('/:id', getEventById);
+router.get("/:id", getEventById);
 
 // Protected routes
 router.use(isAuthenticated);
-router.post('/', upload.array('eventImages', 5), createEvent);
-router.put('/:id', upload.array('eventImages', 5), updateEvent);
-router.delete('/:id', deleteEvent);
-router.post('/:id/join', joinEvent);
-router.post('/:id/leave', leaveEvent);
-router.post('/:id/teams', addTeam);
-router.post('/:id/ratings', addRating);
-router.get('/user/my-events', getUserEvents);
+router.post("/", upload.array("eventImages", 5), createEvent);
+router.put("/:id", upload.array("eventImages", 5), updateEvent);
+router.delete("/:id", deleteEvent);
+router.post("/:id/join", joinEvent);
+router.post("/:id/leave", leaveEvent);
+router.post("/:id/teams", addTeam);
+router.post("/:id/ratings", addRating);
+router.get("/user/my-events", getUserEvents);
 
 export default router;

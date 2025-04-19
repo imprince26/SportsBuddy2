91be71cc7e2abc3142418c7eea46590bb3b1410a
imprince26 <princepatel260806@gmail.com>
2025-04-20 00:37:30 +0530

    Update Code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -108,67 +108,67 @@ function App() {
           </ProtectedRoute>
         }
       />
-        <Route
-                    path="events/:id/teams"
-                    element={
-                      <ProtectedRoute>
-                        <TeamManagement />
-                      </ProtectedRoute>
-                    }
-                  />
+      <Route
+        path="events/:id/teams"
+        element={
+          <ProtectedRoute>
+            <TeamManagement />
+          </ProtectedRoute>
+        }
+      />
 
-<Route
-                    path="users/:id/followers"
-                    element={
-                      <ProtectedRoute>
-                        <FollowersFollowing type="followers" />
-                      </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="users/:id/following"
-                    element={
-                      <ProtectedRoute>
-                        <FollowersFollowing type="following" />
-                      </ProtectedRoute>
-                    }
-                  />
-                         <Route
-                    path="settings"
-                    element={
-                      <ProtectedRoute>
-                        <Settings />
-                      </ProtectedRoute>
-                    }
-                  />
+      <Route
+        path="users/:id/followers"
+        element={
+          <ProtectedRoute>
+            <FollowersFollowing type="followers" />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="users/:id/following"
+        element={
+          <ProtectedRoute>
+            <FollowersFollowing type="following" />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="settings"
+        element={
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>
+        }
+      />
 
       <Route path='/' element={<AdminLayout />}>
 
-      {/* Admin Routes */}
-      <Route
-        path="admin/users"
-        element={
-          <ProtectedRoute adminOnly>
-            <ManageUsers />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="admin/events"
-        element={
-          <ProtectedRoute adminOnly>
-            <ManageEvents />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="admin/notifications"
-        element={
-          <ProtectedRoute adminOnly>
-            <ManageNotifications />
-          </ProtectedRoute>
-        }
-      />
+        {/* Admin Routes */}
+        <Route
+          path="admin/users"
+          element={
+            <ProtectedRoute adminOnly>
+              <ManageUsers />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="admin/events"
+          element={
+            <ProtectedRoute adminOnly>
+              <ManageEvents />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="admin/notifications"
+          element={
+            <ProtectedRoute adminOnly>
+              <ManageNotifications />
+            </ProtectedRoute>
+          }
+        />
       </Route>
 
       {/* Fallback Route */}

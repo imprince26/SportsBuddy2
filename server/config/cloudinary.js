2f90a75ee8a77aa68bc2d66c0330cfe379b6c87c
imprince26 <princepatel260806@gmail.com>
2025-05-15 17:20:02 +0530

    format code

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,4 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
+import {CloudinaryStorage} from 'multer-storage-cloudinary';
+import multer from 'multer';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,6 +9,19 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'SportsBuddy-2',
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+    public_id: (req, file) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      return `${file.fieldname}-${uniqueSuffix}`;
+    },
+  },
 });
 
 export default cloudinary;

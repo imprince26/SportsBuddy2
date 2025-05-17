6b297914cf3d6ae46ba30eee887d505118be1aa1
imprince26 <princepatel260806@gmail.com>
2025-05-17 17:27:16 +0530

    update code

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -84,30 +84,6 @@ const uploadImage = async (file, folder = "SportsBuddy-2") => {
   }
 }
 
-// const handleUploadError = (err, req, res, next) => {
-//   if (err instanceof multer.MulterError) {
-//     if (err.code === 'LIMIT_FILE_SIZE') {
-//       return res.status(400).json({
-//         success: false,
-//         message: 'File size too large. Maximum size is 5MB.',
-//       });
-//     }
-//     if (err.code === 'LIMIT_FILE_COUNT') {
-//       return res.status(400).json({
-//         success: false,
-//         message: 'Too many files. Maximum is 5 files.',
-//       });
-//     }
-//   }
 
-//   if (err.message.includes('Invalid file type')) {
-//     return res.status(400).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-
-//   next(err);
-// }
 
 export { upload, deleteImage, uploadImage, cloudinary };

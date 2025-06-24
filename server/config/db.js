cde1b33a0f0442ba4484dd6706b337335fd194fb
imprince26 <princepatel260806@gmail.com>
2025-06-24 15:56:06 +0530

    Update Code

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected Successfully`);
   } catch (error) {
     console.error(`Error: ${error.message}`);

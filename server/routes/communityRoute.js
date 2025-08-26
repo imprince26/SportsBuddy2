1504467b23b1884774e1c943c0ceefdafbaab6d4
imprince26 <princepatel260806@gmail.com>
2025-08-26 23:41:54 +0530

    added apis and createcommunity page

--- server/routes/communityRoute.js
+++ server/routes/communityRoute.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import {
-    getCommunityPosts,
-    createCommunityPost,
-    getCommunityPostById,
-    updateCommunityPost,
-    deleteCommunityPost,
-    likeCommunityPost,
-    addCommentToPost,
-    getCommunityStats,
-    getTrendingPosts,
-    getFollowingPosts
+  createCommunity,
+  getCommunities,
+  getCommunity,
+  updateCommunity,
+  deleteCommunity,
+  getUserCommunities,
+  getFeaturedCommunities,
+  searchCommunities,
+  joinCommunity,
+  leaveCommunity,
+  getCommunityPosts,
+  createCommunityPost,
+  getCommunityPostById,
+  updateCommunityPost,
+  deleteCommunityPost,
+  likeCommunityPost,
+  addCommentToPost,
+  getCommunityStats,
+  getTrendingPosts,
+  getFollowingPosts
 } from '../controllers/communityController.js';
 import { isAuthenticated } from '../middleware/authMiddleware.js';
 import { upload } from '../config/cloudinary.js';
@@ -17,13 +27,31 @@ import { upload } from '../config/cloudinary.js';
 const router = express.Router();
 
 // Public routes
+router.get('/', getCommunities);
+router.get('/featured', getFeaturedCommunities);
+router.get('/search', searchCommunities);
+router.get('/stats', getCommunityStats);
 router.get('/posts', getCommunityPosts);
 router.get('/posts/trending', getTrendingPosts);
 router.get('/posts/:id', getCommunityPostById);
-router.get('/stats', getCommunityStats);
+router.get('/:id', getCommunity);
 
 // Protected routes
 router.use(isAuthenticated);
+
+// Community CRUD
+router.post('/', upload.array('image', 1), createCommunity);
+router.put('/:id', upload.array('image', 1), updateCommunity);
+router.delete('/:id', deleteCommunity);
+
+// User communities
+router.get('/user/:userId?', getUserCommunities);
+
+// Community membership
+router.post('/:id/join', joinCommunity);
+router.post('/:id/leave', leaveCommunity);
+
+// Community posts
 router.post('/posts', upload.array('images', 5), createCommunityPost);
 router.put('/posts/:id', upload.array('images', 5), updateCommunityPost);
 router.delete('/posts/:id', deleteCommunityPost);

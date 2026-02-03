8d45242e27695da306c3ae6552179850c8200148
imprince26 <princepatel260806@gmail.com>
2026-02-03 23:57:10 +0530

    fix: community details skeleton

--- client/src/components/community/details/CommunityDetailsSkeleton.jsx
+++ client/src/components/community/details/CommunityDetailsSkeleton.jsx
@@ -32,7 +32,7 @@ const CommunityDetailsSkeleton = () => {
             </div>
 
             {/* Main Content */}
-            <div className="container mx-auto px-4 pb-8">
+            <div className="container mx-auto mt-10 px-4 pb-8">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                     {/* Main Area */}
                     <div className="lg:col-span-2 space-y-6">
